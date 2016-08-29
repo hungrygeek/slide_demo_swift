06d94c8de3d6da5eb2f0fa851a6f008122941568
Shi Shu <s.shu@wustl.edu>
2016-08-29 12:04:50 -0500

    Initial Commit

---
+++ lecture1_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lecture1_demo
+//
+//  Created by ShiShu on 8/29/16.
+//  Copyright © 2016 hungrygeek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
