56f4e6fa7fc30b9bcbc41a8f148f7664557345be
Shi Shu <s.shu@wustl.edu>
2016-08-31 11:31:29 -0500

    Slider demo complete

--- lecture1_demo/ViewController.swift
+++ lecture1_demo/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var theLabel: UILabel!
+    @IBOutlet var theSlider: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func drag(sender: UISlider) {
+        theLabel.text = String(sender.value)
+    }
 
 }
 
